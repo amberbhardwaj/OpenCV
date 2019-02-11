fb6177e4405b6b21b348cbb8e7e8057149556563
Amber Bhardwaj <amber.bhardwaj10@gmail.com>
2019-02-11 16:33:23 +0530

    QR code reader/Writer
    Signed-off-by: Amber Bhardwaj <amber.bhardwaj10@gmail.com>

---
+++ QRReader.py
@@ -0,0 +1,15 @@
+#DECODER
+
+#import qrtools
+#qr = qrtools.QR()
+#data='honeywell.png'
+#qr.decode(data)
+#print qr.data
+
+###ENCODER
+
+#import qrcode
+#img = qrcode.make('C:\Users\H307839\Desktop\OneDriveBackup\OneDrive - Honeywell\Hackathon\a.png')
+#img.show()
+
+##DIsTANCE
