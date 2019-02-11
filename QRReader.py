f067702787e2a6c54e5a19334db52023fa85340f
Amber Bhardwaj <amber.bhardwaj10@gmail.com>
2019-02-11 16:36:26 +0530

    Update QRReader.py
    added description to choose the decoder/encoder

--- QRReader.py
+++ QRReader.py
@@ -1,8 +1,9 @@
+# Enable the code based on requirement
 #DECODER
 
 #import qrtools
 #qr = qrtools.QR()
-#data='honeywell.png'
+#data='xyz.png'
 #qr.decode(data)
 #print qr.data
 
@@ -11,5 +12,3 @@
 #import qrcode
 #img = qrcode.make('C:\Users\H307839\Desktop\OneDriveBackup\OneDrive - Honeywell\Hackathon\a.png')
 #img.show()
-
-##DIsTANCE
